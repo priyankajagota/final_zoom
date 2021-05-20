4fbf4cfa96f09bf43ba4e9daf77323e2558de946
priyankajagota <60633744+priyankajagota@users.noreply.github.com>
2021-05-20 12:27:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import keras
 
 
 
-@st.cache(allow_output_mutation=True, max_entries=100, ttl=10800,suppress_st_warning=True)
+#@st.cache(allow_output_mutation=True, max_entries=100, ttl=10800,suppress_st_warning=True)
 
 def foo():
     st.image ("ezgif.com-gif-maker.gif")
@@ -26,7 +26,7 @@ def foo():
 
 from PIL import Image, ImageOps
 import numpy as np
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
+#@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
 
 def ImageClassificationModel(img,model):
     # Step 1
@@ -48,7 +48,7 @@ def ImageClassificationModel(img,model):
     # Step 5
     prediction = model.predict(data)
     return np.argmax(prediction) # return position of the highest probability
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
+#@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
 
 def bar():
     #st.write(' It will predict the presence of Pituitary tumor, Meningioma tumor and Glioma tumors in the brain')
@@ -63,7 +63,7 @@ def bar():
             st.write("Brain tumor is present")
         else:
             st.write("Brain tumor is not present")
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
+#@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
 
 def ImageClassificationModel1(img,model):
         model = keras.models.load_model('my_model_MRI_Jupy_types_of_cancer.h5')
@@ -82,7 +82,7 @@ def ImageClassificationModel1(img,model):
         prediction = model.predict(data)
         return np.argmax(prediction) # return position of the highest probability
 
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
+#@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
 def bar1():
          st.write(' It will predict the presence of Pituitary tumor, Meningioma tumor and Glioma tumors in the brain')
          uploaded_file = st.file_uploader("Please Choose MRI image of brain", type=["jpg","jpeg"])
@@ -103,7 +103,7 @@ def bar1():
                     st.write("Meningioma tumor is  present")
              elif label==3:
                    st.write(" Glioma tumor  is  present")
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
+#@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=100, ttl=10800)
 
 def Author():
     st.image ("MEDIC CARE.png")
