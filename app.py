6be9b74bba4249efc9a5ab77621eee072ceca7a0
priyankajagota <60633744+priyankajagota@users.noreply.github.com>
2021-05-20 11:24:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import keras
 
 
 
-#@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600,suppress_st_warning=True)
+@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600,suppress_st_warning=True)
 #@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600)
 def foo():
     st.image ("ezgif.com-gif-maker.gif")
@@ -27,7 +27,7 @@ def foo():
 
 from PIL import Image, ImageOps
 import numpy as np
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,allow_input_mutation=True)
+@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,ignore_hash=True)
 #@st.cache(suppress_st_warning=True)
 def ImageClassificationModel(img,model):
     # Step 1
@@ -51,7 +51,7 @@ def ImageClassificationModel(img,model):
     # Step 5
     prediction = model.predict(data)
     return np.argmax(prediction) # return position of the highest probability
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,allow_input_mutation=True)
+@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,ignore_hash=True)
 #@st.cache(suppress_st_warning=True)
 def bar():
     #st.write(' It will predict the presence of Pituitary tumor, Meningioma tumor and Glioma tumors in the brain')
@@ -66,7 +66,7 @@ def bar():
             st.write("Brain tumor is present")
         else:
             st.write("Brain tumor is not present")
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,allow_input_mutation=True)
+@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,ignore_hash=True)
 #@st.cache(suppress_st_warning=True)
 def ImageClassificationModel1(img,model):
         model = keras.models.load_model('my_model_MRI_Jupy_types_of_cancer.h5')
@@ -85,7 +85,7 @@ def ImageClassificationModel1(img,model):
         prediction = model.predict(data)
         return np.argmax(prediction) # return position of the highest probability
 #@st.cache(suppress_st_warning=True)
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,allow_input_mutation=True)
+@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,ignore_hash=True)
 def bar1():
          st.write(' It will predict the presence of Pituitary tumor, Meningioma tumor and Glioma tumors in the brain')
          uploaded_file = st.file_uploader("Please Choose MRI image of brain", type=["jpg","jpeg"])
@@ -106,7 +106,7 @@ def bar1():
                     st.write("Meningioma tumor is  present")
              elif label==3:
                    st.write(" Glioma tumor  is  present")
-@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,allow_input_mutation=True)
+@st.cache(suppress_st_warning=True,allow_output_mutation=True, max_entries=10, ttl=3600,ignore_hash=True)
 #@st.cache(suppress_st_warning=True)
 def Author():
     st.image ("MEDIC CARE.png")
