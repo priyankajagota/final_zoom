96523a8d6471b27d3e0a40764bb2c5d6c28bbbc8
priyankajagota <60633744+priyankajagota@users.noreply.github.com>
2021-05-19 21:12:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -108,9 +108,9 @@ def ImageClassificationModel1(img,model):
         # Step 3
         image_array = np.asarray(image)
         # Normalize the image
-        #Normalize_image = (image_array.astype(np.float32) / 127.0) - 1
+        Normalize_image = (image_array.astype(np.float32) / 127.0) - 1
         # Step 4
-        data[0] = image_array
+        data[0] = Normalize_image
         # Step 5
         prediction = model.predict(data)
         return np.argmax(prediction) # return position of the highest probability
