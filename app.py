2cde03e004995d2cedef7606ebb730e4649f9d81
priyankajagota <60633744+priyankajagota@users.noreply.github.com>
2025-03-25 22:46:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def ImageClassificationModel1(img,model):
 def bar1():
             # Dashboard Main Panel
             # Dashboard Main Panel
-    col = st.columns((1.5, 4.5, 2), gap='medium')
+    col = st.columns((1.5, 4.5, 2), gap='medium', vertical_alignment="bottom", border=True)
     with col[1]:
             st.markdown('##### BRAIN TUMOR PREDICTOR')
             uploaded_file = st.file_uploader("Upload MRI image of Brain", type=["jpg","jpeg"])
@@ -109,7 +109,7 @@ def bar1():
     with col[2]:
                with st.expander('About', expanded=True):
                     st.write('''
-                 - Devolper Desk: Brain Tumors are one of the deadliest diseases. It's important to diagnose it at a very early stage.Magnetic Resonance Imaging (MRI) is the most widely used method to identify brain tumors. With the help of Medicinal Robot, MRI images are analyzed to predict the presence or absence of Brain Tumor. This application has above 95% accuracy. However, before getting to any conclusion, please consult doctors first. You are using this web app at your own risk!
+                 - :red[**Developer Desk**]: Brain Tumors are one of the deadliest diseases. It's important to diagnose it at a very early stage.Magnetic Resonance Imaging (MRI) is the most widely used method to identify brain tumors. With the help of Medicinal Robot, MRI images are analyzed to predict the presence or absence of Brain Tumor. This application has above 95% accuracy. However, before getting to any conclusion, please consult doctors first. You are using this web app at your own risk!
                  - :red[**Problem Statement**]: It can distinguish among Pituitary tumor, Meningioma tumor, and Glioma tumor.
                  - :red[**How to Use**]: Upload the MRI scan of brain in either JPG or JPEG format. 
                  ''')
